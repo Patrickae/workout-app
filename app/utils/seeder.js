459b02c166f0e35c600007ec4cae33dacbf681f9
Andy <andyepatrick@gmail.com>
2017-07-27 15:46:40 -0400

    commit back exercises

--- app/utils/seeder.js
+++ app/utils/seeder.js
@@ -443,7 +443,86 @@ var shoulderExercisesList = [
 
 ];
 
+	var backExercisesList = [
+	{
+		exerciseName: "Barbell Bent Over Row",
+		URL: "http://www.exrx.net/WeightExercises/BackGeneral/BBBentOverRow.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/Bent_Over_Barbell_Row.png",
+		mechanics:"Compound"
+	},
+	{
+		exerciseName: "Seated Row",
+		URL: "http://www.exrx.net/WeightExercises/BackGeneral/CBSeatedRow.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/Seated_Low_Cable_Row.png",
+		mechanics:"Compound"
+	},
+	{
+		exerciseName: "Dumbbell Bent Over Row",
+		URL: "http://www.exrx.net/WeightExercises/BackGeneral/DBBentOverRow.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/One_Arm_Dumbbell_Row.png",
+		mechanics:"Compound"
+	},
+	{
+		exerciseName: "T-bar Row",
+		URL: "http://www.exrx.net/WeightExercises/BackGeneral/LVTBarRow.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/Bent_Over_Two-Arm_Long_Bar_Row.png",
+		mechanics:"Compound"
+	},
+	{
+		exerciseName: "Assisted Pull-up",
+		URL: "http://www.exrx.net/WeightExercises/LatissimusDorsi/AsPullupKneeling.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/Machine_Assisted_Pull-Ups_Pullups-1.png",
+		mechanics:"Compound"
+	},
+	{
+		exerciseName: "Pull-up",
+		URL: "http://www.exrx.net/WeightExercises/LatissimusDorsi/BWPullup.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/Pullup.png",
+		mechanics:"Compound"
+	},
+	{
+		exerciseName: "Close Grip Pulldown",
+		URL: "http://www.exrx.net/WeightExercises/LatissimusDorsi/CBCloseGripPulldown.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/V-Bar_Triangle_Bar_Lat_Pull_Downs.png",
+		mechanics:"Compound"
+	},
+	{
+		exerciseName: "Close Grip Pulldown",
+		URL: "http://www.exrx.net/WeightExercises/LatissimusDorsi/CBCloseGripPulldown.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/V-Bar_Triangle_Bar_Lat_Pull_Downs.png",
+		mechanics:"Compound"
+	},
+	{
+		exerciseName: "Cable Pulldown",
+		URL: "http://www.exrx.net/WeightExercises/LatissimusDorsi/CBFrontPulldown.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/Wide-Grip_Lat_Pulldown.png",
+		mechanics:"Compound"
+	},
+	{
+		exerciseName: "Barbell Shrug",
+		URL: "http://www.exrx.net/WeightExercises/TrapeziusUpper/BBShrug.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/Barbell_Shrug.png",
+		mechanics:"Isolated"
+	},
+	{
+		exerciseName: "Cable Wide Grip Seated Row",
+		URL: "http://www.exrx.net/WeightExercises/BackGeneral/CBWideGripSeatedRow.html",
+		muscleGroup:"Back",
+		image: "http://workoutlabs.com/wp-content/uploads/watermarked/Seated_Machine_Back_Row.png",
+		mechanics:"Compound"
+	}
 
+];
 
 
 
