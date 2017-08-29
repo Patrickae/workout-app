38d129614f38cae58e9600153ca4d208ce25f84f
Andy <andyepatrick@gmail.com>
2017-08-29 08:37:44 -0400

    cleaned up

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 
 //importin routes from the routes file in the config folder
 import App from "./config/App";
-import Home from "./components/Home"
 import { HashRouter } from 'react-router-dom'
 
 render((<HashRouter><App /></HashRouter>), document.getElementById("app"));
